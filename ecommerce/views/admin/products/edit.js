29ec29bcb5f0cc452288929fc8dfe994558fab60
nareshreddysama <31837144+nareshreddysama@users.noreply.github.com>
2020-08-24 12:06:36 -0700

    Add files via upload
    Vanilla Js eCommerce project

---
+++ ecommerce/views/admin/products/edit.js
@@ -0,0 +1,34 @@
+const layout = require('../layout');
+const {getError} = require('../../helpers');
+
+module.exports = ({product,errors})=>{
+    return layout({content:`
+    <div class="columns is-centered">
+      <div class="column is-half">
+        <h1 class="subtitle">Update a Product</h1>
+
+        <form method="POST" enctype="multipart/form-data">
+          <div class="field">
+            <label class="label">Title</label>
+            <input class="input" placeholder="Title" name="title" value = "${product.title}">
+            <p class="help is-danger">${getError(errors, 'title')}</p>
+          </div>
+          
+          <div class="field">
+            <label class="label">Price</label>
+            <input class="input" placeholder="Price" name="price" value = "${product.price}">
+            <p class="help is-danger">${getError(errors, 'price')}</p>
+          </div>
+          
+          <div class="field">
+            <label class="label">Image</label>            
+            <input type="file" name="image" />
+          </div>
+          <br />
+          <button class="button is-primary">Update</button>
+        </form>
+      </div>
+    </div>
+  `
+    });
+};
